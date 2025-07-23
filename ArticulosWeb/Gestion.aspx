ad54e04fe1fafc7e455493a3321bb798bbba9611
agust <agustin198ar@gmail.com>
2025-07-23 17:30:48 -0300

    Gestion

--- ArticulosWeb/Gestion.aspx
+++ ArticulosWeb/Gestion.aspx
@@ -1,4 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Gestion.aspx.cs" Inherits="ArticulosWeb.Gestion" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="FullWidthContent" runat="server">
@@ -6,5 +7,26 @@
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
     <h2>Bienvenido Admin</h2>
     <h3>Proximadamente agregaremos un panel administrativo para usted 😋</h3>
+
+    <!-- dgv -->
+    <asp:GridView ID="dgvLista" runat="server" DataKeyNames="Id"
+        CssClass="table"
+        AutoGenerateColumns="false"
+        AllowPaging="True"
+        PageSize="5"
+        PagerStyle-CssClass="pagination"
+        PagerStyle-HorizontalAlign="Center"
+        OnSelectedIndexChanged="DgvLista_SelectedIndexChanged"
+        OnPageIndexChanging="DgvLista_PageIndexChanging">
+        <Columns>
+            <asp:BoundField HeaderText="Email" DataField="Email" />
+            <asp:BoundField HeaderText="Nombre" DataField="Nombre" />
+            <asp:BoundField HeaderText="Apellido" DataField="Apellido" />
+            <asp:BoundField HeaderText="Admin" DataField="Admin" />
+            <asp:CommandField HeaderText="Accion" ShowSelectButton="true" SelectText="✍️" />
+        </Columns>
+    </asp:GridView>
+    <a href="FormularioArticulo.aspx" class="btn btn-primary">Agregar</a>
+
 </asp:Content>
 
--- ArticulosWeb/Gestion.aspx.cs
+++ ArticulosWeb/Gestion.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using NegocioArticulo;
 
 namespace ArticulosWeb
 {
@@ -11,7 +13,50 @@ namespace ArticulosWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
 
+                Negocio articulo = new Negocio();
+                List<Articulo> lista = articulo.listarConSP();
+                Session["listaArticulos"] = lista;
+                dgvLista.DataSource = lista;
+                dgvLista.DataBind();
+
+                // Si el filtro avanzado esta activo, inicializamos el criterio
+
+            }
+        }
+        protected void DgvLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string id = dgvLista.SelectedDataKey.Value.ToString();
+            Response.Redirect("FormularioArticulo.aspx?id=" + id);
+        }
+        protected void DgvLista_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            dgvLista.PageIndex = e.NewPageIndex;
+            if (chkAvanzado.Checked)
+            {
+                // Filtro avanzado
+                dgvLista.DataSource = Negocio.filtrar(
+                    ddlCampo.SelectedItem.ToString(),
+                    ddlCriterio.SelectedItem.ToString(),
+                    txtFiltroAvanzado.Text);
+            }
+            else if (!string.IsNullOrEmpty(txtFiltro.Text))
+            {
+                // Filtro simple
+                List<Articulo> lista = (List<Articulo>)Session["listaArticulos"];
+                List<Articulo> listaFiltrada = lista.FindAll(x => x.Nombre.ToUpper().Contains(txtFiltro.Text.ToUpper()));
+                dgvLista.DataSource = listaFiltrada;
+            }
+            else
+            {
+                // Sin filtro
+                List<Articulo> lista = (List<Articulo>)Session["listaArticulos"];
+                dgvLista.DataSource = lista;
+            }
+
+            dgvLista.DataBind();
         }
     }
 }
