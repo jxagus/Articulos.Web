21647dcaeeb194311c2638d853a279e5ae4ca973
agust <agustin198ar@gmail.com>
2025-07-25 20:45:20 -0300

    generic handler

---
+++ ArticulosWeb/SubirImagen.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArticulosWeb
+{
+    /// <summary>
+    /// Descripción breve de SubirImagen
+    /// </summary>
+    public class SubirImagen : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hola a todos");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
