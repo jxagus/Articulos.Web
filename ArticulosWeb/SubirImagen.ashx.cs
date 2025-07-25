09b7ebea4ab542d2208c9ffff92dd0a3bf7e97f6
agust <agustin198ar@gmail.com>
2025-07-25 20:46:08 -0300

    ihttphandler: subir imagen

--- ArticulosWeb/SubirImagen.ashx.cs
+++ ArticulosWeb/SubirImagen.ashx.cs
@@ -1,28 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
+using System.IO;
 
-namespace ArticulosWeb
+public class SubirImagen : IHttpHandler
 {
-    /// <summary>
-    /// Descripción breve de SubirImagen
-    /// </summary>
-    public class SubirImagen : IHttpHandler
+    public void ProcessRequest(HttpContext context)
     {
+        context.Response.ContentType = "application/json";
 
-        public void ProcessRequest(HttpContext context)
+        var file = context.Request.Files["upload"];
+        if (file != null && file.ContentLength > 0)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hola a todos");
+            string nombreArchivo = Path.GetFileName(file.FileName);
+            string rutaRelativa = "~/ImagenesUploads/" + nombreArchivo;
+            string rutaFisica = context.Server.MapPath(rutaRelativa);
+
+            Directory.CreateDirectory(Path.GetDirectoryName(rutaFisica));
+            file.SaveAs(rutaFisica);
+
+            string urlImagen = context.Request.Url.GetLeftPart(UriPartial.Authority) + context.Request.ApplicationPath.TrimEnd('/') + "/ImagenesUploads/" + nombreArchivo;
+
+            context.Response.Write("{ \"uploaded\": 1, \"fileName\": \"" + nombreArchivo + "\", \"url\": \"" + urlImagen + "\" }");
         }
-
-        public bool IsReusable
+        else
         {
-            get
-            {
-                return false;
-            }
+            context.Response.Write("{ \"uploaded\": 0, \"error\": { \"message\": \"No se recibió imagen\" } }");
         }
     }
+
+    public bool IsReusable => false;
 }
