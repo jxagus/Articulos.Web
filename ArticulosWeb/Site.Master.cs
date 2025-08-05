4248a6f4f254612c7b4480570661813cf50d909f
agust <agustin198ar@gmail.com>
2025-08-05 18:24:45 -0300

    page is carrito

--- ArticulosWeb/Site.Master.cs
+++ ArticulosWeb/Site.Master.cs
@@ -18,7 +18,7 @@ namespace ArticulosWeb
                 imgAvatar.ImageUrl = "~/Img/usuarioDefault.jpg";
 
                 // Pags que no requieren sesion
-                if (!(Page is Login || Page is Registro || Page is Explorar || Page is Preguntas || Page is Error || Page is Default || Page is Lista || Page is DetalleArticulo))
+                if (!(Page is Login || Page is Registro || Page is Explorar || Page is Preguntas || Page is Error || Page is Default || Page is Lista || Page is DetalleArticulo || Page is Carrito))
                 {
                     if (!Seguridad.sesionActiva(Session["usuario"]))
                     {
