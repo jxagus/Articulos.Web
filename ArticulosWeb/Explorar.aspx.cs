2913a2229fa418c8a4cb9edbf1d03bf81d13923e
agust <agustin198ar@gmail.com>
2025-08-07 19:32:51 -0300

    categoriaContada + selectedindexchanged Ordendar

--- ArticulosWeb/Explorar.aspx.cs
+++ ArticulosWeb/Explorar.aspx.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using NegocioArticulo;
 using Dominio;
-using System.Web.UI.HtmlControls;
-using System.Globalization;
-
 
 namespace ArticulosWeb
 {
@@ -20,48 +16,84 @@ namespace ArticulosWeb
         {
             if (!IsPostBack)
             {
+                CargarArticulos();
+
+                // Ordenamiento inicial o por búsqueda
                 if (Session["txtBuscar"] != null)
                 {
                     TextBox txtBuscarNav = (TextBox)Master.FindControl("txtBuscarNav");
                     if (txtBuscarNav != null)
-                    {
                         txtBuscarNav.Text = Session["txtBuscar"].ToString();
-                    }
 
-                    string filtro = Session["txtBuscar"].ToString().ToLower();
-                    List<Articulo> lista = (List<Articulo>)Session["ListaArticulos"];
-                    List<Articulo> filtrada = lista.FindAll(a =>
-                        a.Nombre.ToLower().Contains(filtro) ||
-                        a.Marca.Descripcion.ToLower().Contains(filtro) ||
-                        a.Categoria.Descripcion.ToLower().Contains(filtro)
-                    );
+                    AplicarFiltro(Session["txtBuscar"].ToString());
+                }
 
-                    RepExplorar.DataSource = filtrada;
-                    RepExplorar.DataBind();
-                }
-                else
-                {
-                    Negocio Articulos = new Negocio();
-                    ListaArticulos = Articulos.listarConSP();
-                    Session["ListaArticulos"] = ListaArticulos;
-                    RepExplorar.DataSource = ListaArticulos;
-                    RepExplorar.DataBind();
-                }
+                CargarCategorias();
             }
         }
 
+        private void CargarArticulos()
+        {
+            Negocio negocio = new Negocio();
+            ListaArticulos = negocio.listarConSP();
+            Session["ListaArticulos"] = ListaArticulos;
 
+            RepExplorar.DataSource = ListaArticulos;
+            RepExplorar.DataBind();
+        }
 
+        private void AplicarFiltro(string filtro)
+        {
+            List<Articulo> lista = (List<Articulo>)Session["ListaArticulos"];
+            filtro = filtro.ToLower();
 
-        //protected void Filtro_TextChanged(object sender, EventArgs e)
-        //{
-        //    List<Articulo> lista = (List<Articulo>)Session["ListaArticulos"];
-        //    List<Articulo> listaFiltrada = lista.FindAll(x => x.Nombre.ToLower().Contains(txtFiltro.Text.ToLower()) ||
-        //    x.Codigo.ToLower().Contains(txtFiltro.Text.ToLower()));
+            List<Articulo> filtrada = lista.FindAll(a =>
+                a.Nombre.ToLower().Contains(filtro) ||
+                a.Marca.Descripcion.ToLower().Contains(filtro) ||
+                a.Categoria.Descripcion.ToLower().Contains(filtro)
+            );
 
-        //    RepExplorar.DataSource = listaFiltrada;
-        //    RepExplorar.DataBind();
-        //}
+            RepExplorar.DataSource = filtrada;
+            RepExplorar.DataBind();
+        }
+
+        private void CargarCategorias()
+        {
+            List<Articulo> lista = (List<Articulo>)Session["ListaArticulos"];
+
+            var categorias = lista
+                .GroupBy(a => a.Categoria.Descripcion)
+                .Select(g => new CategoriaContada
+                {
+                    Descripcion = g.Key,
+                    Cantidad = g.Count()
+                }).ToList();
+
+            RepCategorias.DataSource = categorias;
+            RepCategorias.DataBind();
+        }
+
+        protected void ddlOrdenar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string criterio = ddlOrdenar.SelectedValue;
+            List<Articulo> articulos = (List<Articulo>)Session["ListaArticulos"];
+
+            switch (criterio)
+            {
+                case "mayor":
+                    articulos = articulos.OrderByDescending(a => a.Precio).ToList();
+                    break;
+                case "menor":
+                    articulos = articulos.OrderBy(a => a.Precio).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            RepExplorar.DataSource = articulos;
+            RepExplorar.DataBind();
+        }
+
         public string ObtenerUrlImagen(object imagen)
         {
             string url = imagen?.ToString();
@@ -69,7 +101,11 @@ namespace ArticulosWeb
                 return "Img/NoDisponible.jpg";
             return url;
         }
+    }
 
-
+    public class CategoriaContada
+    {
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
     }
 }
