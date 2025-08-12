2fa74de03800c377e5af0571a6e66e8e42192ded
agust <agustin198ar@gmail.com>
2025-08-11 22:13:26 -0300

    funcion cargarrelacionados()

--- ArticulosWeb/Carrito.aspx.cs
+++ ArticulosWeb/Carrito.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +15,21 @@ namespace ArticulosWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        private void CargarRelacionados()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["TuConexion"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Nombre, Descripcion, ImagenUrl FROM Articulos";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                rptRelacionados.DataSource = dt;
+                rptRelacionados.DataBind();
+            }
         }
     }
 }
