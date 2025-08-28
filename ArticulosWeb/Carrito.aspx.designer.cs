a3fe315726c7c25e4a723d7082d7d128ae0ad314
agust <agustin198ar@gmail.com>
2025-08-28 17:38:28 -0300

    Lista de productos + total

--- ArticulosWeb/Carrito.aspx.designer.cs
+++ ArticulosWeb/Carrito.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace ArticulosWeb
 
     public partial class Carrito
     {
+
+        /// <summary>
+        /// Control rptCarrito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptCarrito;
+
+        /// <summary>
+        /// Control lblTotal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTotal;
     }
 }
