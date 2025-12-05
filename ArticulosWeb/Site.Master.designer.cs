32f694e7ef42e45484d2a99ed5656b952c351ea0
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-12-05 11:11:02 -0300

    nueva seccion

--- ArticulosWeb/Site.Master.designer.cs
+++ ArticulosWeb/Site.Master.designer.cs
@@ -78,6 +78,15 @@ namespace ArticulosWeb
         protected global::System.Web.UI.WebControls.ContentPlaceHolder OutsideContainer;
 
         /// <summary>
+        /// Control OutsideContainer2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder OutsideContainer2;
+
+        /// <summary>
         /// Control FullWidthContent.
         /// </summary>
         /// <remarks>
